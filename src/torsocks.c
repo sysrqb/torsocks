438370705f73e4139d7eb8d27936698d99fa77d9
Matthew Finkel <Matthew.Finkel@gmail.com>
2013-03-02 22:28:43 +0000

    Use appropriate errno for read/readv if SOCKS handshake fails

--- src/torsocks.c
+++ src/torsocks.c
@@ -1275,7 +1275,7 @@ ssize_t torsocks_read_guts(READ_SIGNATURE, ssize_t(*original_read)(READ_SIGNATUR
 	handle_request(conn);
 
         if (conn->state != DONE) {
-            errno = ENOTCONN;
+            errno = EBADF;
             return(-1);
         }
     }
@@ -1350,7 +1350,7 @@ ssize_t torsocks_readv_guts(READV_SIGNATURE, ssize_t(*original_readv)(READV_SIGN
 	handle_request(conn);
 
         if (conn->state != DONE) {
-            errno = ENOTCONN;
+            errno = EBADF;
             return(-1);
         }
     }
