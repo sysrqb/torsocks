b0f1eef9ee1cdfec1248f0f2d6e209811ef447eb
Matthew Finkel <Matthew.Finkel@gmail.com>
2015-07-26 20:02:26 +0000

    Rename replace for select

--- src/common/connection.h
+++ src/common/connection.h
@@ -90,7 +90,8 @@ struct connection *connection_find(int key);
 void connection_destroy(struct connection *conn);
 void connection_remove(struct connection *conn);
 void connection_insert(struct connection *conn);
-int connection_conn_list_find_and_replace(fd_set *fds, int ***replaced, int *len);
+int connection_conn_list_find_and_replace_select(fd_set *fds, int ***replaced,
+						int *len);
 
 void connection_registry_lock(void);
 void connection_registry_unlock(void);
