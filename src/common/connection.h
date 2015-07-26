9049baa03c0a315c91d7b0a9fdaf4490e1f33e07
Matthew Finkel <Matthew.Finkel@gmail.com>
2015-07-26 20:02:57 +0000

    Hijack poll() and ppoll()

--- src/common/connection.h
+++ src/common/connection.h
@@ -22,6 +22,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <poll.h>
 
 #include "defaults.h"
 #include "ht.h"
@@ -92,6 +93,8 @@ void connection_remove(struct connection *conn);
 void connection_insert(struct connection *conn);
 int connection_conn_list_find_and_replace_select(fd_set *fds, int ***replaced,
 						int *len);
+void connection_conn_list_find_and_replace_poll(struct pollfd *fds, nfds_t nfds,
+						int ***replaced, int *len);
 
 void connection_registry_lock(void);
 void connection_registry_unlock(void);
