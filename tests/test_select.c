4ed763adbe7e554713f0d0b7ee7035f6669d28fd
Matthew Finkel <Matthew.Finkel@gmail.com>
2015-08-07 04:55:01 +0000

    Add pselect tests

--- tests/test_select.c
+++ tests/test_select.c
@@ -41,10 +41,10 @@
 		now = time(NULL);			\
 	} while (0)
 
-#define TEST1_TESTS()					\
+#define TEST1_TESTS(vers)				\
 	do {						\
 		ok(ret != -1 && (time(NULL) - now) < 2,	\
-		   "Select returned without error");	\
+		   #vers " returned without error");	\
 		ok(!FD_ISSET(pipe_fds[0], &readfds),	\
 		   "Read end of pipe has no data");	\
 		ok(FD_ISSET(pipe_fds[1], &writefds),	\
@@ -63,10 +63,10 @@
 		now = time(NULL);		\
 	} while (0)				\
 
-#define TEST2_TESTS()						\
+#define TEST2_TESTS(vers)					\
 	do {							\
 		ok(ret != -1 && (time(NULL) - now) < 2,		\
-		   "Select returned without error, again");	\
+		   #vers " returned without error, again");	\
 		ok(FD_ISSET(pipe_fds[0], &readfds),		\
 		   "Read end of pipe has data");		\
 		ok(FD_ISSET(pipe_fds[1], &writefds),		\
@@ -121,10 +121,10 @@
 		now = time(NULL);				\
 	} while (0)
 
-#define TEST3_TESTS()						\
+#define TEST3_TESTS(vers)					\
 	do {							\
 		ok(ret != -1 && (time(NULL) - now) < 2,		\
-		   "Select with inet socket returned without "	\
+		   #vers " with inet socket returned without "	\
 		   "error");					\
 		ok(!FD_ISSET(inet_sock, &readfds),		\
 		   "inet socket has no data needing reading");	\
@@ -143,10 +143,10 @@
 		now = time(NULL);		\
 	} while (0)
 
-#define TEST4_TESTS()						\
+#define TEST4_TESTS(vers)					\
 	do {							\
 		ok(ret != -1 && (time(NULL) - now) < 2,		\
-		   "Select with inet socket returned without "	\
+		   #vers " with inet socket returned without "	\
 		   "error");					\
 		ok(!FD_ISSET(inet_sock, &readfds),		\
 		   "inet socket has no data needing reading");	\
@@ -165,10 +165,10 @@
 		now = time(NULL);		\
 	} while (0)
 
-#define TEST567_TESTS()						\
+#define TEST567_TESTS(vers)					\
 	do {							\
 		ok(ret != -1 && (time(NULL) - now) < 2,		\
-		   "Select with inet socket returned without "	\
+		   #vers " with inet socket returned without "	\
 		   "error");					\
 		ok(!FD_ISSET(inet_sock, &readfds),		\
 		   "inet socket has no data needing reading");	\
@@ -269,33 +269,99 @@ static void test_select(void)
 
 	/* Now let's see if we return immediately and successfully */
 	ret = select(pipe_fds[1] + 1, &readfds, &writefds, &exceptfds, &tv);
-	TEST1_TESTS();
+	TEST1_TESTS(select);
 
 	TEST2_SETUP(usec);
 	ret = select(pipe_fds[1] + 1, &readfds, &writefds, &exceptfds, &tv);
-	TEST2_TESTS();
+	TEST2_TESTS(select);
 
 	PIPE_SETS_REFRESH();
 
 	TEST3_SETUP(usec);
 	ret = select(inet_sock + 1, &readfds, &writefds, &exceptfds, &tv);
-	TEST3_TESTS();
+	TEST3_TESTS(select);
 
 	TEST4_SETUP(usec);
 	ret = select(inet_sock + 1, &readfds, &writefds, &exceptfds, &tv);
-	TEST4_TESTS();
+	TEST4_TESTS(select);
 
 	TEST5_SETUP(usec);
 	ret = select(inet_sock + 1, &readfds, &writefds, &exceptfds, &tv);
-	TEST567_TESTS();
+	TEST567_TESTS(select);
 
 	TEST6_SETUP(usec);
 	ret = select(inet_sock2 + 1, &readfds, &writefds, &exceptfds, &tv);
-	TEST567_TESTS();
+	TEST567_TESTS(select);
 
 	TEST7_SETUP(usec);
 	ret = select(inet_sock2 + 1, &readfds, &writefds, &exceptfds, &tv);
-	TEST567_TESTS();
+	TEST567_TESTS(select);
+
+
+error:
+	if (inet_sock >= 0) {
+		close(inet_sock);
+	}
+	if (inet_sock2 >= 0) {
+		close(inet_sock);
+	}
+	if (inet_sock3 >= 0) {
+		close(inet_sock);
+	}
+	return;
+}
+
+static void test_pselect(void)
+{
+	int pipe_fds[2], ret;
+	int inet_sock = -1, inet_sock2 = -1, inet_sock3 = -1;
+	struct sockaddr_in addrv4;
+	const char *ip = "93.95.227.222";
+	fd_set readfds, writefds, exceptfds;
+	struct timespec tv;
+	time_t now;
+
+	ret = pipe(pipe_fds);
+	if (ret < 0) {
+		fail("Unable to create pipe");
+		goto error;
+	}
+
+	/* This test is to see if we go through the libc or not. */
+	ret = getpeername(pipe_fds[0], NULL, NULL);
+	ok(ret == -1 && errno == ENOTSOCK, "Invalid socket fd");
+
+        TEST1_SETUP(nsec);
+
+	/* Now let's see if we return immediately and successfully */
+	ret = pselect(pipe_fds[1] + 1, &readfds, &writefds, &exceptfds, &tv, NULL);
+	TEST1_TESTS(pselect);
+
+	TEST2_SETUP(nsec);
+	ret = pselect(pipe_fds[1] + 1, &readfds, &writefds, &exceptfds, &tv, NULL);
+	TEST2_TESTS(pselect);
+
+	PIPE_SETS_REFRESH();
+
+	TEST3_SETUP(nsec);
+	ret = pselect(inet_sock + 1, &readfds, &writefds, &exceptfds, &tv, NULL);
+	TEST3_TESTS(pselect);
+
+	TEST4_SETUP(nsec);
+	ret = pselect(inet_sock + 1, &readfds, &writefds, &exceptfds, &tv, NULL);
+	TEST4_TESTS(pselect);
+
+	TEST5_SETUP(nsec);
+	ret = pselect(inet_sock + 1, &readfds, &writefds, &exceptfds, &tv, NULL);
+	TEST567_TESTS(pselect);
+
+	TEST6_SETUP(nsec);
+	ret = pselect(inet_sock2 + 1, &readfds, &writefds, &exceptfds, &tv, NULL);
+	TEST567_TESTS(pselect);
+
+	TEST7_SETUP(nsec);
+	ret = pselect(inet_sock2 + 1, &readfds, &writefds, &exceptfds, &tv, NULL);
+	TEST567_TESTS(pselect);
 
 
 error:
@@ -314,9 +380,10 @@ error:
 int main(int argc, char **argv)
 {
 	/* Libtap call for the number of tests planned. */
-	plan_tests(NUM_TESTS);
+	plan_tests(NUM_TESTS*2);
 
 	test_select();
+	test_pselect();
 
     return 0;
 }
