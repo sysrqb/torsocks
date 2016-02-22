bd1a0cbafcf314da6c5edc75bf9118c775ec9e1b
Matthew Finkel <Matthew.Finkel@gmail.com>
2016-02-22 08:40:41 +0000

    Improve socket log message

--- src/lib/socket.c
+++ src/lib/socket.c
@@ -24,15 +24,104 @@
 /* socket(2) */
 TSOCKS_LIBC_DECL(socket, LIBC_SOCKET_RET_TYPE, LIBC_SOCKET_SIG)
 
+#define CASE_DOMAIN(d)		\
+	case d:			\
+		name = #d;	\
+		break
+
+#define SOCKET_TYPE(t)					\
+	if ((type & t) == 0) {				\
+		name = #t;				\
+	} else if ((type & (t | SOCK_NONBLOCK)) == 0) {	\
+		name = #t " | SOCK_NONBLOCK";		\
+	} else if ((type & (t | SOCK_CLOEXEC)) == 0) {	\
+		name = #t " | SOCK_CLOEXEC";		\
+	}
+
+static
+char * domain_str(int domain, char *buf, size_t buflen)
+{
+	char *name;
+	switch (domain) {
+	CASE_DOMAIN(AF_UNIX|AF_LOCAL);
+	CASE_DOMAIN(AF_INET);
+	CASE_DOMAIN(AF_INET6);
+	CASE_DOMAIN(AF_IPX);
+	CASE_DOMAIN(AF_APPLETALK);
+#if defined(__linux__)
+	CASE_DOMAIN(AF_NETLINK);
+	CASE_DOMAIN(AF_X25);
+	CASE_DOMAIN(AF_AX25);
+	CASE_DOMAIN(AF_ATMPVC);
+	CASE_DOMAIN(AF_PACKET);
+#endif
+	default:
+		name = "AF not recognized";
+	}
+
+	buflen = buflen > strlen(name)?strlen(name):buflen;
+	strncpy(buf, name, buflen);
+	return buf;
+}
+
+static
+char * type_str(int type, char *buf, size_t buflen)
+{
+	char *name = "AF not recognized";
+	SOCKET_TYPE(SOCK_STREAM)
+	else SOCKET_TYPE(SOCK_DGRAM)
+	else SOCKET_TYPE(SOCK_SEQPACKET)
+	else SOCKET_TYPE(SOCK_RAW)
+	else SOCKET_TYPE(SOCK_RDM)
+#if defined(__linux__)
+	else SOCKET_TYPE(SOCK_PACKET)
+#endif
+
+	buflen = buflen > strlen(name)?strlen(name):buflen;
+	strncpy(buf, name, buflen);
+	return buf;
+}
+
+static
+char * proto_str(int protocol, char *buf, size_t buflen)
+{
+	char *name;
+	struct protoent *ent;
+
+	ent = getprotobynumber(protocol);
+	if (ent == NULL)
+		name = "protocol not recognized";
+	else
+		name = ent->p_name;
+	endprotoent();
+
+	buflen = buflen > strlen(name)?strlen(name):buflen;
+	strncpy(buf, name, buflen);
+	return buf;
+}
 /*
  * Torsocks call for socket(2)
  */
 LIBC_SOCKET_RET_TYPE tsocks_socket(LIBC_SOCKET_SIG)
 {
 	int fd;
+	char descr_buf1[20];
+	char descr_buf2[20];
+	char descr_buf3[20];
 
-	DBG("[socket] Creating socket with domain %d, type %d and protocol %d",
-			domain, type, protocol);
+	/* Only do this if the debug message is printed. */
+	if (tsocks_loglevel >= MSGDEBUG) {
+		memset(descr_buf1, 0, sizeof(descr_buf1));
+		memset(descr_buf2, 0, sizeof(descr_buf2));
+		memset(descr_buf3, 0, sizeof(descr_buf3));
+	}
+
+	DBG("[socket] Creating socket with domain %s (%d), type %s (%d) and "
+		"protocol %s (%d)",
+			domain_str(domain, descr_buf1, sizeof(descr_buf1)),
+			domain, type_str(type, descr_buf2, sizeof(descr_buf2)),
+			type, proto_str(protocol, descr_buf3, sizeof(descr_buf3)),
+			protocol);
 
 	if (IS_SOCK_STREAM(type)) {
 		/*
@@ -69,7 +158,7 @@ LIBC_SOCKET_RET_TYPE tsocks_socket(LIBC_SOCKET_SIG)
 		 * output.
 		 */
 		DBG("IPv4/v6 non TCP socket denied. Tor network can't handle it.");
-		errno = EPERM;
+		errno = EACCES;
 		return -1;
 
 	}
